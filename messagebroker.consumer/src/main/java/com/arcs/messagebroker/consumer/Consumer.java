d072fb6dbd10bd71ada909f29738917e7be1bfc8
Alysson <alysson_salgado@hotmail.com>
2020-11-06 15:29:15 -0300

    Comentários

--- messagebroker.consumer/src/main/java/com/arcs/messagebroker/consumer/Consumer.java
+++ messagebroker.consumer/src/main/java/com/arcs/messagebroker/consumer/Consumer.java
@@ -2,6 +2,7 @@ package com.arcs.messagebroker.consumer;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.rabbitmq.client.CancelCallback;
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
@@ -10,39 +11,81 @@ import com.rabbitmq.client.DeliverCallback;
 @SpringBootApplication
 public class Consumer {
 
-	private final static String QUEUE_NAME = "hello";
+	// DECLARAÇÃO DA FILA QUE O RABBIT MQ IRÁ RECEBER
+	// O NOME DA FILA SERÁ IGUAL NO RECEBEDOR
+	private final static String QUEUE_NAME = "RABBIT_MQ_QUEUE";
+
+	// ENDEREÇO DO SERVIDOR ONDE ESTÁ O RABBIT MQ
+	private final static String HOST_ADDRESS = "localhost";
 
 	public void consumeMessage() throws Exception {
+
+		// CLASSE PARA FACILITAR A CONEXÃO COM O RABBIT MQ
 		ConnectionFactory factory = new ConnectionFactory();
-		factory.setHost("localhost");
+
+		// DEFININDO O HOST
+		factory.setHost(HOST_ADDRESS);
+
+		// INTERFACE DE CONEXÃO
 		Connection connection = factory.newConnection();
+
+		// INTERFACE PARA COMUNICAR COM O RABBIT MQ
 		Channel channel = connection.createChannel();
 
+		// DECLARAÇÃO DA FILA
+		// ELA SÓ SERÁ CRIADA SE AINDA NÃO EXISTIR
 		channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+
+		// LOG
 		System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
-		
+
+		// QUANTIDADE DE MENSAGENS QUE IRÁ RECEBER POR VEZ
 		channel.basicQos(1);
-		
+
+		// FUNÇÃO QUE SERÁ ACIONADA SEMPRE QUE RECEBERMOS UMA MENSAGEM
 		DeliverCallback deliverCallback = (consumerTag, delivery) -> {
 
+			// CAPTURANDO O CORPO DA MENSAGEM
 			String message = new String(delivery.getBody(), "UTF-8");
 
+			// LOG
 			System.out.println(" [x] Received '" + message + "'");
+			
 			try {
+				// QUANDO RECEBER A MENSAGEM, FAÇA O QUE TIVER QUE SER FEITO
 				doWork(message);
 			} catch (InterruptedException e) {
+				// LOG
 				System.out.println(" [x] Error");
 			} finally {
+				// LOG
 				System.out.println(" [x] Done");
-			    channel.basicAck(delivery.getEnvelope().getDeliveryTag(), false);
+				
+				// AVISANDO O RABBIT MQ QUE O A MENSAGEM FOI RECEBIDA POR COMPLETO
+				channel.basicAck(delivery.getEnvelope().getDeliveryTag(), false);
 			}
 		};
 
+		// FUNÇÃO QUE SERÁ ACIONADA SEMPRE QUE A MENSAGEM FOR CANCELADA
+		CancelCallback cancelCallback = (consumerTag) -> {
+			
+		};
+
+		// CONFIRMADOR DE MENSAGEM RECEBIDA
+		// CASO TRUE, ASSIM QUE O RABBIT MQ NOS ENTREGA A MENSAGEM
+		// ELE JÁ MARCA PARA EXCLUSÃO LÁ
+		// CASO FALSE, O RABBIT ESPERA UMA CONFIRMAÇÃO NOSSA 
+		// DE QUE A MENSAGEM FOI RECEBIDA
+		
+		// ACK = ACKNOWLEDGE = RECONHECIMENTO DA MENSAGEM RECEBIDA 
 		boolean autoAck = false;
-		channel.basicConsume(QUEUE_NAME, autoAck, deliverCallback, consumerTag -> {
-		});
+		
+		// INICIA UM CONSUMIDOR, PASSANDO A FILA, O AUTOACK E O CALLBACK DE ENTREGA E CANCELAMENTO
+		channel.basicConsume(QUEUE_NAME, autoAck, deliverCallback, cancelCallback);
 	}
 
+	// CLASSE SIMULANDO O TEMPO DE PROCESSAMENTO DE UMA MENSAGEM
+	// A CADA '.' (PONTO) NA MENSAGEM, O "PROCESSAMENTO" DEMORA MAIS 1 SEGUNDO
 	private static void doWork(String task) throws InterruptedException {
 		for (char ch : task.toCharArray()) {
 			if (ch == '.')
--- messagebroker.producer/src/main/java/com/arcs/messagebroker/producer/Send.java
+++ messagebroker.producer/src/main/java/com/arcs/messagebroker/producer/Send.java
@@ -9,15 +9,38 @@ import com.rabbitmq.client.ConnectionFactory;
 @SpringBootApplication
 public class Send {
 
-	private final static String QUEUE_NAME = "hello";
+	// DECLARAÇÃO DA FILA QUE O RABBIT MQ IRÁ RECEBER
+	// O NOME DA FILA SERÁ IGUAL NO RECEBEDOR
+	private final static String QUEUE_NAME = "RABBIT_MQ_QUEUE";
+	
+	// ENDEREÇO DO SERVIDOR ONDE ESTÁ O RABBIT MQ
+	private final static String HOST_ADDRESS = "localhost";
 
 	public void sendMessage(String message) throws Exception {
+		
+		// CLASSE PARA FACILITAR A CONEXÃO COM O RABBIT MQ
 		ConnectionFactory factory = new ConnectionFactory();
-		factory.setHost("localhost");
+		
+		// DEFININDO O HOST
+		factory.setHost(HOST_ADDRESS);
+		
+		// UTILIZANDO O TRY-WITH-RESOURCES PARA QUE A CONEXÃO FECHE AUTOMATICAMENTE AO ENVIAR A MENSAGEM
+		// AS CLASSES IMPLEMENTAM JAVA.IO.CLOSEABLE
 		try (Connection connection = factory.newConnection(); Channel channel = connection.createChannel()) {
+
+			// DECLARAÇÃO DA FILA
+			// ELA SÓ SERÁ CRIADA SE AINDA NÃO EXISTIR
 			channel.queueDeclare(QUEUE_NAME, false, false, false, null);
-			String mensagem = String.join ( "" , message); 
+			
+			// MENSAGEM A SER ENVIADA
+			String mensagem = String.join ( "" , message);
+			
+			// PUBLICANDO A MENSAGEM PARA O RABBIT MQ
+			// O CONTEÚDO DA MENSAGEM É UMA MATRIZ DE BYTES
+			// ENTÃO PODE-SE CODIFICAR O QUE QUISER
 			channel.basicPublish ( "" , QUEUE_NAME , null , message.getBytes ()); 
+
+			// LOG
 			System.out.println ( "[x] Enviado '" + mensagem + "'" );
 		}
 	}
